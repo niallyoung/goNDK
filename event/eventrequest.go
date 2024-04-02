7b111230425769fb35d57299f9f03abf28457be0
Niall Young <5465765+niallyoung@users.noreply.github.com>
2024-04-02 18:58:22 +1100

    clarification: Event (domain/backend) != EventRequest (http/ws handler)

---
+++ event/eventrequest.go
@@ -0,0 +1,25 @@
+package event
+
+import "time"
+
+// EventRequest is intended for relay handlers and their concerns:
+//   - receipt, initial validation, etc. before transformation to the full Event type
+type EventRequest struct {
+	ID        string    `json:"id"`
+	PubKey    string    `json:"pubkey"`
+	CreatedAt Timestamp `json:"created_at"`
+	Kind      int       `json:"kind"`
+	Tags      Tags      `json:"tags"`
+	Content   string    `json:"content"`
+	Sig       string    `json:"sig"`
+}
+
+type Timestamp int64
+
+func (t Timestamp) Time() time.Time {
+	return time.Unix(int64(t), 0)
+}
+
+func Now() Timestamp {
+	return Timestamp(time.Now().Unix())
+}
