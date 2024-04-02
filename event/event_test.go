2bf2a77201161c0a74ae48e0b7ab31ca31fe1eaa
Niall Young <5465765+niallyoung@users.noreply.github.com>
2024-04-02 18:57:52 +1100

    WIP: event_test.go progress

--- event/event_test.go
+++ event/event_test.go
@@ -9,9 +9,24 @@ import (
 	"github.com/niallyoung/goNDK/event"
 )
 
+var validEvent = event.NewEvent("fake-pubkey", time.Unix(0, 0), 1, event.Tags{}, "fake-content", "fake-sig")
+
 func TestNewEvent(t *testing.T) {
 	t.Run("NewEvent() returns an Event", func(t *testing.T) {
-		e := event.NewEvent("fake-pubkey", time.Unix(0, 0), 1, event.Tags{}, "fake-content", "fake-sig")
+		e := validEvent
 		assert.NotNil(t, e)
+	})
+
+	t.Run("new event has an ID", func(t *testing.T) {
+		e := validEvent
+		assert.Equal(t, "9d004c6d691bb765165f30dfa8854355033025b12f564707331d8de5f0a77a72", e.ID())
+	})
+}
+
+func TestEvent_Validate(t *testing.T) {
+	t.Run("validate", func(t *testing.T) {
+		e := validEvent
+		err := e.Validate()
+		assert.NoError(t, err)
 	})
 }
